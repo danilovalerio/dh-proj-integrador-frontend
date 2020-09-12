10425ae9a70990d34c6040d73084a2781342f8c9
Danilo Valério da Silva <danilo_vs@hotmail.com>
2020-09-11 21:52:21 -0300

    adicao da estrutura basica e responsiva dos componentes main e footer

--- coworking-app/src/app/template/template.module.ts
+++ coworking-app/src/app/template/template.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
+import { MainComponent } from './main/main.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
-  declarations: [NavbarComponent],
+  declarations: [NavbarComponent, MainComponent, FooterComponent],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [NavbarComponent, MainComponent, FooterComponent]
 })
 export class TemplateModule { }
