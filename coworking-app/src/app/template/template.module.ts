a8aea5306497371d1a492f973a5084153e9ee1de
Danilo Valério da Silva <danilo_vs@hotmail.com>
2020-09-11 22:13:19 -0300

    component itemcard

--- coworking-app/src/app/template/template.module.ts
+++ coworking-app/src/app/template/template.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { MainComponent } from './main/main.component';
 import { FooterComponent } from './footer/footer.component';
+import { CardItemComponent } from './card-item/card-item.component';
 
 
 
 @NgModule({
-  declarations: [NavbarComponent, MainComponent, FooterComponent],
+  declarations: [NavbarComponent, MainComponent, FooterComponent, CardItemComponent],
   imports: [
     CommonModule
   ],
