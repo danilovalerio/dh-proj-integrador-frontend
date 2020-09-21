2516fbaacce52a86b2124d0442ead5b58450b757
Danilo Valério da Silva <danilo_vs@hotmail.com>
2020-09-21 19:58:29 -0300

    imagem em base64 de exemplo

--- coworking-app/src/app/sala.ts
+++ coworking-app/src/app/sala.ts
@@ -3,10 +3,12 @@ export class Sala {
     id: number;
     nome: string;
     limitePessoas: number;
+    foto: String;
 
-    constructor(id: number, nome: string, limitePessoas: number) {
+    constructor(id: number, nome: string, limitePessoas: number, foto: String) {
         this.id = id;
         this.nome = nome;
         this.limitePessoas = limitePessoas;
+        this.foto = foto;
     }
 }
