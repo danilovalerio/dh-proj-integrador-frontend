9aada372c4d4e79d0b3b85a9267da7145965a0e4
gc-pereira <carvalho.gabrielp@gmail.com>
2020-10-11 00:03:49 -0300

    custom navbar and main page

--- coworking-app/src/app/template/navbar/navbar.component.ts
+++ coworking-app/src/app/template/navbar/navbar.component.ts
@@ -22,6 +22,28 @@ export class NavbarComponent implements OnInit {
         document.getElementById("logo").style.width = "170px";
       }
     }
+
+    function myFunction() {
+      var x = document.getElementById("myTopnav");
+      if (x.className === "topnav") {
+        x.className += " responsive";
+      } else {
+        x.className = "topnav";
+      }
+    }
+
+    const menuBtn = document.querySelector('.menu-btn');
+    let menuOpen = false;
+    menuBtn.addEventListener('click', () => {
+      if(!menuOpen){
+          menuBtn.classList.add('open');
+          menuOpen = true;
+      }else{
+          menuBtn.classList.remove('open');
+          menuOpen = false;
+      }
+    });
+
   }
 
 }
